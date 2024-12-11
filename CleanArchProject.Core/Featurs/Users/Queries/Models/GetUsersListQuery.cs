14fffd1c2df9fd2007bba4d12f12a03e0fff2bea
MalekNoureddine <noureddinemalek50@gmail.com>
2024-12-11 19:04:12 +0100

    Add Query operations Endpoints for the IdentityUser

---
+++ CleanArchProject.Core/Featurs/Users/Queries/Models/GetUsersListQuery.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchProject.Core.Featurs.Users.Queries.Responses;
+using CleanArchProject.Data.Enums;
+using MediatR;
+using SchoolProject.Core.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchProject.Core.Featurs.Users.Queries.Models
+{
+    public class GetUsersListQuery : IRequest<PaginatedResult<GetUsersListResponse>>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+    }
+}
