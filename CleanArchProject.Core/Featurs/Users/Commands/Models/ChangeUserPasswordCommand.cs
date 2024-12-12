509b643fced72c558d299b211156a4d76e2c2aab
MalekNoureddine <noureddinemalek50@gmail.com>
2024-12-12 11:27:18 +0100

    Add Edit user and change user password rndpoints

---
+++ CleanArchProject.Core/Featurs/Users/Commands/Models/ChangeUserPasswordCommand.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchProject.Core.Bases;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchProject.Core.Featurs.Users.Commands.Models
+{
+    public class ChangeUserPasswordCommand : IRequest<Response<string>>
+    {
+        public ChangeUserPasswordCommand(int id, string currentPassword, string newPassword, string confirmPassword)
+        {
+            Id = id;
+            CurrentPassword = currentPassword;
+            NewPassword = newPassword;
+            ConfirmPassword = confirmPassword;
+        }
+
+        public int Id { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ CleanArchProject.Core/Featurs/Users/Commands/Validators/ChangeUserPasswordValidator.cs
@@ -0,0 +1,52 @@
+﻿using CleanArchProject.Core.Featurs.Users.Commands.Models;
+using CleanArchProject.Core.SharedResources;
+using CleanArchProject.Data.Entities.Identities;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchProject.Core.Featurs.Users.Commands.Validators
+{
+    public class ChangeUserPasswordValidator : AbstractValidator<ChangeUserPasswordCommand>
+    {
+        #region Fields
+        private readonly IStringLocalizer<SharedResources.SharedResources> _localizer;
+        private readonly UserManager<User> _userManager;
+
+        #endregion
+
+        #region Constructors
+        public ChangeUserPasswordValidator(IStringLocalizer<SharedResources.SharedResources> localizer, UserManager<User> userManager)
+        {
+            _localizer = localizer;
+            _userManager = userManager;
+            ApplyValidationsRules();
+        }
+        #endregion
+
+        #region Handle Functions
+        public void ApplyValidationsRules()
+        {
+            RuleFor(x => x.Id)
+                 .NotEmpty().WithMessage(_localizer[SharedResourcesKeys.NotEmpty])
+                 .NotNull().WithMessage(_localizer[SharedResourcesKeys.Required]);
+            RuleFor(x => x.CurrentPassword)
+                 .NotEmpty().WithMessage(_localizer[SharedResourcesKeys.NotEmpty])
+                 .NotNull().WithMessage(_localizer[SharedResourcesKeys.Required]);
+
+            RuleFor(x => x.NewPassword)
+                 .NotEmpty().WithMessage(_localizer[SharedResourcesKeys.NotEmpty])
+                 .NotNull().WithMessage(_localizer[SharedResourcesKeys.Required]);
+            RuleFor(x => x.ConfirmPassword)
+                 .Equal(x => x.NewPassword).WithMessage(_localizer[SharedResourcesKeys.PasswordNotEqualConfirmPass]);
+
+        }
+
+        #endregion
+    }
+}
