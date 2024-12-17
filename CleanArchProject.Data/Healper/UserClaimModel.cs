5cb9cb6859f38ea56be9b926f71bfa2d28097a44
MalekNoureddine <noureddinemalek50@gmail.com>
2024-12-17 09:41:45 +0100

    Adding validating tokens and refreshToken endpoints

--- CleanArchProject.Data/Healper/UserClaimModel.cs
+++ CleanArchProject.Data/Healper/UserClaimModel.cs
@@ -8,6 +8,7 @@ namespace CleanArchProject.Data.Healper
 {
     public class UserClaimModel
     {
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
